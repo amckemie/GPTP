aa3c5ffb175ec5edc5e8bd00d52babaa3a5faa15
Tony Fazio <antoniojfazio@gmail.com>
2014-05-23 11:57:07 -0500

    penny list to penny profile works

--- gptp_sinatra.rb
+++ gptp_sinatra.rb
@@ -73,6 +73,7 @@ post '/penny_profile' do
   @where = params[:location]
   @how_long = params[:time_requirement]
   @organization =params[:organization]
+  @id = params[:id]
   erb :penny_profile
 end
 
